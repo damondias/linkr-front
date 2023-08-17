c783fc35e05eaaf1de68e29c263747249a97df6f
JeanPiton <jean.orpiton@gmail.com>
2023-08-17 12:10:14 -0300

    Fix: render in sign up and login page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react"
 import { NavBarBody, NavBarOptions, NavBarUser } from "../styles/NavBarStyle"
+import { useLocation } from "react-router-dom"
 
 export default function NavBar(){
     const [hidden,setHidden] = useState(true)
+    const location = useLocation()
+
+    if(location.pathname=="/sign-up"||location.pathname=="/") return
 
     return(
         <NavBarBody>

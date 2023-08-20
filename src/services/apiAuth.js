6d937d68bd0559f58c5dd466577f36198dca4e43
Deborah Wyny <129215842+deborahwyny@users.noreply.github.com>
2023-08-20 18:53:27 -0300

    cadastro post

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -8,7 +8,7 @@ async function useSignUp() {
     const navigate = useNavigate()
 
     return (body) => {
-        axios.post(`${process.env.REACT_APP_API_URL}/sign-up`, body)
+        axios.post(`${process.env.REACT_APP_API_URL}/cadastro`, body)
             .then(res => navigate("/"))
             .catch(err => alert(err.response.data))
     }
